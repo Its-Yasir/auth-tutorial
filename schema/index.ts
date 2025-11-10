8ac04cd668a33b626a88c93acbc9617c5a09ea0d
Its-Yasir <iamyasirtariq@gmail.com>
2025-11-11 04:34:22 +0500

    Added Prisma

--- schema/index.ts
+++ schema/index.ts
@@ -11,11 +11,9 @@ export const LoginSchema = z.object({
 export const RegisterSchema = z.object({
   email: z.string().email({
     message:"Email is required!"
-  }).min(6,{
-    error:"6 characters required in password!"
   }),
-  password: z.string().min(1,{
-    message:"Password is required!"
+  password: z.string().min(6,{
+    error:"6 characters required in password!"
   }),
   name: z.string({
     error:"Name is required!"
