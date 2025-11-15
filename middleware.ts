92614a075fe851e4992715141c8d432c86d71bba
Its-Yasir <iamyasirtariq@gmail.com>
2025-11-15 12:05:39 +0500

    Added middle ware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,41 @@
 import authConfig from "@/auth.config"
 import NextAuth from "next-auth"
 import { NextRequest } from "next/server"
-
+import {
+  authRoutes,
+  publicRoutes,
+  apiAuthRoutes,
+  DEFAULT_LOGIN_REDIRECT,
+} from "@/routes";
+import type { Session } from "next-auth"
+import LoginPage from "./app/auth/login/page";
 // Use only one of the two middleware options below
 // 1. Use middleware directly
 // export const { auth: middleware } = NextAuth(authConfig)
 
 // 2. Wrapped middleware option
 const { auth } = NextAuth(authConfig)
-export default auth(async function middleware(req: NextRequest) {
-  console.log("Route: ", req.nextUrl.pathname);
-})
+export default auth(async function middleware(req: NextRequest & {auth: Session | null}) {
+  const {nextUrl} = req;
+  const isLoggedIn = !!req.auth;
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthRoutes);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if(isApiAuthRoute) {
+    return null;
+  }
+  if(isAuthRoute) {
+    if(isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+    }
+    return null;
+  }
+  if(!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL('/auth/login', nextUrl));
+  }
+  return null;
+}) 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
---
+++ routes.ts
@@ -0,0 +1,35 @@
+/**
+ * These routes are acessible to the public
+ * These routes don't require authentication
+ * @type {string[]}
+ */
+
+export const publicRoutes = [
+  "/"
+];
+
+/**
+ * These routes are used for authentication
+ * These routes will redirect user to settings page
+ * @type {string[]}
+ */
+
+export const authRoutes = [
+  '/auth/login',
+  '/auth/register'
+]
+
+/**
+ * The prefix for API authentication routes
+ * Routes that start with prefix are used for API authentication purpose
+ * @type {string}
+ */
+
+export const apiAuthRoutes = "/api/auth";
+
+/**
+ * User is redirected to this page after logging in
+ * @type {string}
+ */
+
+export const  DEFAULT_LOGIN_REDIRECT = '/settings';
