dbb8aba07e84a0d450fbefb4b4403d96efdd2b88
Its-Yasir <iamyasirtariq@gmail.com>
2025-11-15 21:06:09 +0500

    added google and github sign in

--- auth.ts
+++ auth.ts
@@ -2,9 +2,38 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import authConfig from "./auth.config"
 import { db } from "@/lib/db"
+import { getUserById } from "./data/user"
+import { UserRole } from "./lib/generated/prisma/enums"
+
+
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: ({
+
+    async session({ token, session }) {
+      console.log("Session toekn: ", session);
+      if(session.user && token.sub) {
+        session.user.id = token.sub;
+      }
+      if(token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+      return session;
+    },
+
+    async jwt({ token }) {
+      if(!token.sub) return token;
+      
+      const existingUser = await getUserById(token.sub);
+
+      if(!existingUser) return token;
+      
+      token.role = existingUser.Role;
+      console.log("Token: ", token);
+      return token;
+    },
+  }),
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
-})
+}) 
--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -3,21 +3,30 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "../ui/button";
+import { signIn } from "next-auth/react";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const Social = () =>{
+
+  const onClick = (provider: "google" | "github") => {
+    signIn(provider,{
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    })
+  }
+
   return (
     <div className="flex w-full justify-center gap-x-7">
       <Button 
         className="w-[42px] h-[42px]"
         variant={'outline'}
-        onClick={() => {}}
+        onClick={() => onClick("google")}
       >
         <FcGoogle className="h-7 w-7" />
       </Button>
       <Button 
         className="w-[42px] h-[42px]"
         variant={'outline'}
-        onClick={() => {}}
+        onClick={() => onClick("github")}
       >
         <FaGithub className="h-7 w-7" />
       </Button>
