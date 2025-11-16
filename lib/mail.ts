493f4f3368bbd060fa133d314c313c53d2f5cedf
Its-Yasir <iamyasirtariq@gmail.com>
2025-11-16 12:05:10 +0500

    shifting to pc

---
+++ lib/mail.ts
@@ -0,0 +1,11 @@
+import { Resend } from 'resend'
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendVerificationEmail = async (
+  email: string,
+  token: string,
+) => {
+  const confirmLink = `http://localhost:3000/auth/new-version?token=${token}`;
+  await resend.emails.send()
+}
