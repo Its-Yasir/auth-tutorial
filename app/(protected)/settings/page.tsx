ca99f38c8b9bff21cde51bf6fd7adac9fd7ced89
Its-Yasir <iamyasirtariq@gmail.com>
2025-11-19 05:52:25 +0500

    Added user using a hook

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,24 +1,26 @@
 "use client";
 import { logout } from '@/actions/logout';
 import { Button } from '@/components/ui/button';
-import {  useSession } from 'next-auth/react';
-//!import {  signOut } from 'next-auth/react';
+import { useCurrentUser } from '@/hooks/use-current-user';
+// import {  useSession } from 'next-auth/react';
+//import {  signOut } from 'next-auth/react';
 import React from 'react'
 
 const SettingsPage = () => {
-  const session  = useSession();
-  
-  //! We can use this to do server thing in client component, like signOut is server componenet 
-  //! OR we can make a file with "use server" and import signOut in that file and import it here
-  //! const onClick = () => {
-  //!   signOut();
-  //! }
+  // const session  = useSession();
+  const user = useCurrentUser();
+
+  // We can use this to do server thing in client component, like signOut is server componenet 
+  // OR we can make a file with "use server" and import signOut in that file and import it here
+  // const onClick = () => {
+  //   signOut();
+  // }
 
   const onClick = () => {
     logout();
   }
   return (
-    <div>{JSON.stringify(session)}
+    <div>{JSON.stringify(user)}
         <Button
           type='submit'
           onClick={onClick}
