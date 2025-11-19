10e464440a1bd58db6ddd9d586b7fbca5a86f378
Its-Yasir <iamyasirtariq@gmail.com>
2025-11-19 05:47:45 +0500

    converted settings to use client

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,25 +1,32 @@
-import { auth, signOut } from '@/auth'
+"use client";
+import { logout } from '@/actions/logout';
 import { Button } from '@/components/ui/button';
+import {  useSession } from 'next-auth/react';
+//!import {  signOut } from 'next-auth/react';
 import React from 'react'
 
-const page = async () => {
-  const user = await auth();
+const SettingsPage = () => {
+  const session  = useSession();
+  
+  //! We can use this to do server thing in client component, like signOut is server componenet 
+  //! OR we can make a file with "use server" and import signOut in that file and import it here
+  //! const onClick = () => {
+  //!   signOut();
+  //! }
+
+  const onClick = () => {
+    logout();
+  }
   return (
-    <div>{JSON.stringify(user)}
-      <form
-        action = {async () => {
-          "use server";
-          await signOut();
-        }}
-      >
+    <div>{JSON.stringify(session)}
         <Button
           type='submit'
+          onClick={onClick}
         >
           Sign Out
         </Button>
-      </form>
     </div>
   )
 }
 
-export default page
+export default SettingsPage
